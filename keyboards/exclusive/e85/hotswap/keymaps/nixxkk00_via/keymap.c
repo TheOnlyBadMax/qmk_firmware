ea36c4a91c766ff36b808e8eb33647ffbc92af94
Nick <nickheggoe@gmail.com>
2020-10-09 01:45:20 +0200

    Update keymap.c

--- keyboards/exclusive/e85/hotswap/keymaps/nixxkk00_via/keymap.c
+++ keyboards/exclusive/e85/hotswap/keymaps/nixxkk00_via/keymap.c
@@ -17,39 +17,39 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ansi_tsangan(
-        KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,   KC_F6,   KC_F7,   KC_F8,     KC_F9,   KC_F10,  KC_F11,  KC_F12,     MO(1),     KC_PSCR, KC_SLCK, KC_PAUS,
+        KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,   KC_F6,   KC_F7,   KC_F8,     KC_F9,   KC_F10,  KC_F11,  KC_F12,     MO(3),     KC_PSCR, KC_SLCK, KC_PAUS,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,            KC_INS,  KC_HOME, KC_PGUP,
+        KC_TAB,      KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_LBRC, KC_RBRC, KC_BSLS,        KC_DEL,  KC_END,  KC_PGDN,
+        KC_CAPS,       KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT, KC_ENT,
+        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                            KC_UP,
+        KC_LCTL,     KC_NO, KC_LALT,                             KC_SPC,                                 KC_RALT,     KC_NO, KC_RCTL,        KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+
+    [1] = LAYOUT_ansi_tsangan(
+        KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,   KC_F6,   KC_F7,   KC_F8,     KC_F9,   KC_F10,  KC_F11,  KC_F12,     MO(3),     KC_PSCR, KC_SLCK, KC_PAUS,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,            KC_INS,  KC_HOME, KC_PGUP,
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,        KC_DEL,  KC_END,  KC_PGDN,
         KC_CAPS,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                            KC_UP,
-        KC_LCTL,     KC_LGUI, KC_LALT,                             KC_SPC,                                 KC_RALT,     KC_RGUI, KC_RCTL,        KC_LEFT, KC_DOWN, KC_RGHT
-    ),
-
-    [1] = LAYOUT_ansi_tsangan(
-        _______,   _______, _______, _______, _______,   _______, _______, _______, _______,   _______, _______, _______, _______,    _______,   _______, _______, _______,
-        RGB_MOD, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______,
-        _______,     _______, _______, _______, RESET, _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______,
-        BL_TOGG,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                            _______,
-        _______,     _______, _______,                            _______,                                 _______,     _______, _______,        _______, _______, _______
+        KC_LCTL,     KC_NO, KC_LALT,                             KC_SPC,                                 KC_RALT,     KC_NO, KC_RCTL,        KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [2] = LAYOUT_ansi_tsangan(
-        _______,   _______, _______, _______, _______,   _______, _______, _______, _______,   _______, _______, _______, _______,    _______,   _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______,
-        _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______,
-        _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                            _______,
-        _______,     _______, _______,                            _______,                                 _______,     _______, _______,        _______, _______, _______
+        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,
+        KC_LGUI,     KC_NO, KC_TRNS,                            KC_TRNS,                                 KC_TRNS,     KC_NO, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [3] = LAYOUT_ansi_tsangan(
-        _______,   _______, _______, _______, _______,   _______, _______, _______, _______,   _______, _______, _______, _______,    _______,   _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______,
-        _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______, _______, _______,
-        _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                            _______,
-        _______,     _______, _______,                            _______,                                 _______,     _______, _______,        _______, _______, _______
+        KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,    KC_TRNS,   KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            KC_NO, KC_NO, KC_NO,
+        KC_NO,     KC_NO, KC_NO, KC_NO, RESET, KC_NO, KC_NO, KC_NO, KC_NO, NK_OFF, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO,
+        KC_NO,       KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, CG_NORM, KC_NO, KC_NO, KC_NO,
+        KC_NO,            KC_NO, KC_NO, TG(1), KC_NO, KC_NO, NK_ON, CG_SWAP, KC_NO, KC_NO, KC_NO, KC_NO,                            KC_NO,
+        KC_NO,     KC_NO, KC_NO,                            KC_NO,                                 KC_NO,     KC_NO, MO(2),        KC_NO, KC_NO, KC_NO
     ),
 
 };
