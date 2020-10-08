0809ef68824852793a498ddedb8284997314b240
Nick <nickheggoe@gmail.com>
2020-10-08 13:35:57 +0200

    Create rules.mk

---
+++ keyboards/exclusive/e85/hotswap/keymaps/nixxkk00_via/rules.mk
@@ -0,0 +1,10 @@
+VIA_ENABLE = yes
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = yes            # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+MIDI_ENABLE = no            # MIDI support
+UNICODE_ENABLE = no         # Unicode
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+AUDIO_ENABLE = no           # Audio output on port C6
